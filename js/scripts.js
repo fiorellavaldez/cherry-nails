65927dd98aee920abee5f23c8d3e36b969ca3f46
Fiore <fiorellavald3z@gmail.com>
2023-10-11 21:56:15 -0300

    agregada consistencia de datos para form finalizar compra

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
 function consisto_string (string) {
 
     if (!string || string.trim() === ' ' ){
-        alert("Error. El nombre no puede estar vacío")
         return false
     }
 
     if (/[^a-zA-Z0-9\s]/.test(string)){
-        alert("Error. El nombre no puede contener caracteres especiales")
         return false
     }
 
@@ -16,7 +14,6 @@ function consisto_string (string) {
 function consisto_email(email){
 
     if (!email.includes('@')){
-        alert("Error. Formato inválido")
         return false
     }
     return true
@@ -24,102 +21,168 @@ function consisto_email(email){
 
 function consisto_dni(dni){
     if (dni.length !== 8 || dni.includes('.') || /[^a-zA-Z0-9\s]/.test(dni) ){
-        alert("Error. Formato inválido")
         return false
     }
     return true
+}
+
+function consisto_telefono(telefono){
+    return /^[0-9\s\-()+]+$/.test(telefono)
 }
 
 function consisto_cp(codigo){
     if (codigo.length !== 4 || codigo.includes('.') || /[^a-zA-Z0-9\s]/.test(codigo) ){
-        alert("Error. Formato inválido")
         return false
     }
     return true
 }
 
-function consisto_num_calle(num){
+function consisto_num(num){
     if (!num || num.includes('.') || /[^a-zA-Z0-9\s]/.test(num) ){
-        alert("Error. Formato inválido o ingreso nulo")
         return false
     }
     return true
 }
 
-function consisto_nro_tarjeta(nro){
-    if (!nro || nro.includes('.') || /[^a-zA-Z0-9\s]/.test(nro) || nro.length !== 16 ){
-        alert("Error. Formato inválido")
-        return false
+let datosComprador = {
+    nombre_comprador: "",
+    apellido_comprador: "",
+    email_comprador: "",
+    dni_comprador: "",
+    telefono_comprador: "",
+    codigo_postal_comprador: "",
+    dir_calle_comprador: "",
+    dir_num_comprador: "",
+    dir_piso_depa: "",
+    provincia_comprador: "",
+    ciudad_comprador:"",
+}
+
+let formulario = document.querySelector('form.grid-finalizar-compra')
+
+formulario.addEventListener('submit',function(event){
+
+    cont_errores = 0
+    
+    datosComprador.nombre_comprador = document.getElementById('nombre-comprador').value
+    datosComprador.apellido_comprador = document.getElementById('apellido-comprador').value
+    datosComprador.email_comprador = document.getElementById('email-comprador').value
+    datosComprador.dni_comprador = document.getElementById('dni-comprador').value
+    datosComprador.telefono_comprador = document.getElementById('telefono-comprador').value
+    datosComprador.codigo_postal_comprador = document.getElementById('codi-post-compra').value
+    datosComprador.dir_calle_comprador = document.getElementById('dire-compra').value
+    datosComprador.dir_num_comprador = document.getElementById('num-dire-compra').value
+    datosComprador.dir_piso_depa = document.getElementById('num-piso-compra').value
+    datosComprador.provincia_comprador = document.getElementById('provincia-compra').value
+    datosComprador.ciudad_comprador = document.getElementById('ciudad-compra').value
+
+
+    if (!consisto_string(datosComprador.nombre_comprador)){
+        cont_errores += 1
+        let ulErrores = document.querySelector('div.error-nombre ul')
+        ulErrores.innerHTML += '<li> Ingreso inválido </li> '
+        document.querySelector('div.error-nombre ul li').style = "color: red"
+        document.querySelector('div.error-nombre ul').style = "list-style: none"
+        document.getElementById('nombre-comprador').style.border = "1px solid red"
     }
-    return true
-}
 
-let nombre
-let apellido
-let email
-let dni
-let telefono
+    if(!consisto_string(datosComprador.apellido_comprador)){
+        cont_errores += 1
+        let ulErrores = document.querySelector('div.error-apellido ul')
+        ulErrores.innerHTML += '<li> Ingreso inválido </li> '
+        document.querySelector('div.error-apellido ul li').style = "color: red"
+        document.querySelector('div.error-apellido ul').style = "list-style: none"
+        document.getElementById('apellido-comprador').style.border = "1px solid red"
 
-let codigoPostal
-let direccion
-let numero_direccion
-let piso_departamento
-let provincia
-let ciudad
+    }
 
-let nroTarjeta
-let nombreTitularTarjeta
+    if(!consisto_email(datosComprador.email_comprador)){
+        cont_errores += 1
+        let ulErrores = document.querySelector('div.error-email ul')
+        ulErrores.innerHTML += '<li> Ingreso inválido </li> '
+        document.querySelector('div.error-email ul li').style = "color: red"
+        document.querySelector('div.error-email ul').style = "list-style: none"
+        document.getElementById('email-comprador').style.border = "1px solid red"
 
+    }
 
-function ingreso_identificacion() {
-    alert("Ingrese datos de identificacion")
-    do{
-        nombre = prompt("Ingrese su nombre")
-    } while (!consisto_string(nombre))
-    do{
-        apellido = prompt("Ingrese su apellido")
-    } while (!consisto_string(apellido))
-    do{
-        email = prompt("Ingrese su email")
-    } while (!consisto_email(email))
-    do{
-        dni = prompt("Ingrese su DNI (sin puntos y/o espacios)")
-    } while (!consisto_dni(dni))
-    telefono = prompt("Ingrese su teléfono")
-}
+    if(!consisto_dni(datosComprador.dni_comprador)){
+        cont_errores += 1
+        let ulErrores = document.querySelector('div.error-dni ul')
+        ulErrores.innerHTML += '<li> Ingreso inválido </li> '
+        document.querySelector('div.error-dni ul li').style = "color: red"
+        document.querySelector('div.error-dni ul').style = "list-style: none"
+        document.getElementById('dni-comprador').style.border = "1px solid red"
 
-function ingreso_envio(){
-    alert("Ingrese datos de envío")
-    do{
-        codigoPostal = prompt("Ingrese su código postal")
-    } while (!consisto_cp(codigoPostal))
-    do{
-        direccion = prompt("Ingrese la calle de su domicilio")
-    } while(!consisto_string(direccion))
-    do{
-        numero_direccion = prompt("Ingrese el número de su domicilio")
-    } while(!consisto_num_calle(numero_direccion))
+    }
 
-    piso_departamento = prompt("Ingrese n° piso o departamento (opcional)")
+    if(!consisto_telefono(datosComprador.telefono_comprador)){
+        cont_errores += 1
+        let ulErrores = document.querySelector('div.error-telefono ul')
+        ulErrores.innerHTML += '<li> Ingreso inválido </li> '
+        document.querySelector('div.error-telefono ul li').style = "color: red"
+        document.querySelector('div.error-telefono ul').style = "list-style: none"
+        document.getElementById('telefono-comprador').style.border = "1px solid red"
+    }
 
-    do{
-        provincia = prompt("Ingrese provincia de residencia")
-    } while(!consisto_string(provincia))
+    if(!consisto_cp(datosComprador.codigo_postal_comprador)){
+        cont_errores += 1
+        let ulErrores = document.querySelector('div.error-codigo-postal ul')
+        ulErrores.innerHTML += '<li> Ingreso inválido </li> '
+        document.querySelector('div.error-codigo-postal ul li').style = "color: red"
+        document.querySelector('div.error-codigo-postal ul').style = "list-style: none"
+        document.getElementById('codi-post-compra').style.border = "1px solid red"
+    }
 
-    do{
-        ciudad = prompt("Ingrese provincia de residencia")
-    } while(!consisto_string(ciudad))
-}
+    if(!consisto_string(datosComprador.dir_calle_comprador)){
+        cont_errores += 1
+        let ulErrores = document.querySelector('div.error-dir-calle ul')
+        ulErrores.innerHTML += '<li> Ingreso inválido </li> '
+        document.querySelector('div.error-dir-calle ul li').style = "color: red"
+        document.querySelector('div.error-dir-calle ul').style = "list-style: none"
+        document.getElementById('dire-compra').style.border = "1px solid red"
 
-function ingreso_datos_pago(){
-    do{
-        nroTarjeta = prompt("Ingrese el nombre de su tarjeta")
-    } while(!consisto_nro_tarjeta(nroTarjeta))
-    do{
-        nombreTitularTarjeta = prompt("Ingrese el nombre del titular de la tarjeta")
-    } while(!consisto_string(nombreTitularTarjeta))
-}
+    }
 
-ingreso_identificacion()
-ingreso_envio()
-ingreso_datos_pago()
+    if(!consisto_num(datosComprador.dir_num_comprador)){
+        cont_errores += 1
+        let ulErrores = document.querySelector('div.error-dir-num ul')
+        ulErrores.innerHTML += '<li> Ingreso inválido </li> '
+        document.querySelector('div.error-dir-num li').style = "color: red"
+        document.querySelector('div.error-dir-num ul').style = "list-style: none"
+        document.getElementById('num-dire-compra').style.border = "1px solid red"
+    }
+
+    if(!consisto_num(datosComprador.dir_piso_depa)){
+        cont_errores += 1
+        let ulErrores = document.querySelector('div.error-dir-piso ul')
+        ulErrores.innerHTML += '<li> Ingreso inválido </li> '
+        document.querySelector('div.error-dir-piso li').style = "color: red"
+        document.querySelector('div.error-dir-piso ul').style = "list-style: none"
+        document.getElementById('num-piso-compra').style.border = "1px solid red"
+    }
+
+    if(!consisto_string(datosComprador.provincia_comprador)){
+        cont_errores += 1
+        let ulErrores = document.querySelector('div.error-provincia ul')
+        ulErrores.innerHTML += '<li> Ingreso inválido </li> '
+        document.querySelector('div.error-provincia li').style = "color: red"
+        document.querySelector('div.error-provincia ul').style = "list-style: none"
+        document.getElementById('provincia-compra').style.border = "1px solid red"
+    }
+
+    if(!consisto_string(datosComprador.ciudad_comprador)){
+        cont_errores += 1
+        let ulErrores = document.querySelector('div.error-ciudad ul')
+        ulErrores.innerHTML += '<li> Ingreso inválido </li> '
+        document.querySelector('div.error-ciudad li').style = "color: red"
+        document.querySelector('div.error-ciudad ul').style = "list-style: none"
+        document.getElementById('ciudad-compra').style.border = "1px solid red"
+    }
+
+    if (cont_errores > 0){
+        event.preventDefault()
+    } else {
+        localStorage.setItem("unComprador" , JSON.stringify(datosComprador))
+    }
+})
